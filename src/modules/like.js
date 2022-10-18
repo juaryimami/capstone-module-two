c2abe26b2ba1cac3b49883dfb48c6b533f457436
juharyimami <juharyimami2011@gmail.com>
2022-10-18 23:49:35 +0300

    fixing of bugs

--- src/modules/like.js
+++ src/modules/like.js
@@ -8,7 +8,7 @@ export const createApp = async () => {
       'Content-type': 'application/json',
     },
     body: JSON.stringify({
-      name: 'newAppArthur',
+      name: 'newAppCapston',
     }),
   });
   const status = await response.text();
@@ -30,10 +30,34 @@ const createLikes = async (id) => {
   return status;
 };
 
+const getLikes = async () => {
+  const response = await fetch(likeURL);
+
+  const status = await response.json();
+  return status;
+};
+
+const distributeLikes = (likesListAPI) => {
+  const likeCounter = [...document.querySelectorAll('.likes')];
+  likesListAPI.forEach((object) => {
+    const id = parseInt(object.item_id, 10);
+    likeCounter[id].textContent = object.likes;
+  });
+};
+
+export const getListLikesAPI = () => {
+  getLikes().then((result) => {
+    distributeLikes(result);
+  });
+};
+
 export const sendLikes = (event) => {
   const isLikeBtn = event.target.classList.contains('icon-heart');
   if (isLikeBtn) {
     const { id } = event.target.dataset;
-    createLikes(id);
+    createLikes(id).then((result) => {
+      getListLikesAPI();
+      return result;
+    });
   }
 };
