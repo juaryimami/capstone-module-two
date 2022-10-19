14105516449eb857efffaa926feab4defcfe5b03
amon-cofie <ddd.amon123@gmail.com>
2022-10-19 14:55:26 +0000

    edit webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,6 @@ module.exports = {
           'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
         ],
       },
       {
