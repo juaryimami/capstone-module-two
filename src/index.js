155dab1723af15500c167fd81035db5be82b6e72
juharyimami <juharyimami2011@gmail.com>
2022-10-19 19:41:13 +0300

    displaying all likes when Dom loaded

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { getData } from './modules/getData.js';
-import { sendLikes } from './modules/like.js';
+import { sendLikes, getListLikesAPI } from './modules/like.js';
 import './style.css';
 
 const container = document.querySelector('.container');
 
 window.addEventListener('load', () => {
   getData();
-  
+  getListLikesAPI();
 });
 
 container.addEventListener('click', (e) => {
